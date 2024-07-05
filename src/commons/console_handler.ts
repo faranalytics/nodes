981baef83a631ec7d7e162134ee843feabbbfaeb
adamjpatterson <adam@farar.net>
2024-07-05 18:21:34 -0400

    0.0.4
    more

--- src/commons/console_handler.ts
+++ src/commons/console_handler.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as stream from 'node:stream';
+import { once } from 'node:events';
 import { $write, Node } from '../node.js';
 
 export class ConsoleHandler extends Node<any, never> {
@@ -10,8 +11,19 @@ export class ConsoleHandler extends Node<any, never> {
                 objectMode: true,
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 write: async (chunk: unknown, encoding: BufferEncoding, callback: (error?: Error | null | undefined) => void) => {
-                    console.log(chunk);
-                    callback();
+                    try {
+                        if (typeof chunk == 'string' || chunk instanceof Buffer) {
+                            if (!process.stdout.write(chunk)) {
+                                await once(process.stdout, 'drain');
+                            }
+                        }
+                        callback();
+                    }
+                    catch (err) {
+                        if (err instanceof Error) {
+                            callback(err);
+                        }
+                    }
                 }
             }
         }));
