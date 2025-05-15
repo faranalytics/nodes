205476f95c2fb171aa5158d11d5929b10f5224fa
Adam Patterson <adam@farar.net>
2025-05-15 16:33:43 -0400

    Set maxListeners on stream.

--- src/node.ts
+++ src/node.ts
@@ -5,88 +5,90 @@ import { ErrorHandler } from './config.js';
 import * as crypto from 'node:crypto';
 
 export interface NodeOptions {
-    id: string;
-    errorHandler: ErrorHandler;
+  id: string;
+  errorHandler: ErrorHandler;
+  maxListeners: number;
 }
 
 export class Node<InT, OutT, StreamT extends Writable | Readable = Writable | Readable> {
 
-    protected _stream: StreamT;
-    protected _queue: Array<InT>;
-    protected _size: number;
-    protected _id: string;
-    protected _errorHandler: ErrorHandler;
+  protected _stream: StreamT;
+  protected _queue: Array<InT>;
+  protected _size: number;
+  protected _id: string;
+  protected _errorHandler: ErrorHandler;
 
-    constructor(stream: StreamT, options?: NodeOptions) {
-        this._stream = stream;
-        this._queue = [];
-        this._size = 0;
-        this._id = options?.id ?? crypto.randomUUID();
-        this._errorHandler = options?.errorHandler ?? Config.errorHandler;
+  constructor(stream: StreamT, options?: NodeOptions) {
+    this._stream = stream;
+    this._queue = [];
+    this._size = 0;
+    this._id = options?.id ?? crypto.randomUUID();
+    this._errorHandler = options?.errorHandler ?? Config.errorHandler;
+    stream.setMaxListeners(options?.maxListeners ?? Config.maxListeners);
 
-        this._stream.on('unpipe', (readable: Readable) => {
-            readable.resume();
-        });
+    this._stream.on('unpipe', (readable: Readable) => {
+      readable.resume();
+    });
 
-        this._stream.once('error', (err: Error) => {
-            if (Config.debug && this._errorHandler) {
-                this._errorHandler(err);
-            }
-            if (this._stream instanceof Readable) {
-                this._stream.unpipe();
-            }
-        });
+    this._stream.once('error', (err: Error) => {
+      if (Config.debug && this._errorHandler) {
+        this._errorHandler(err);
+      }
+      if (this._stream instanceof Readable) {
+        this._stream.unpipe();
+      }
+    });
+  }
+
+  public connect(...nodes: Array<Node<OutT, unknown>>): typeof this {
+    for (const node of nodes) {
+      if (this._stream instanceof Readable && node._stream instanceof Writable) {
+        this._stream?.pipe(node._stream);
+        if (node._stream instanceof Readable) {
+          node._stream.resume();
+        }
+        if (Config.debug) {
+          console.log(`Connected ${this._id} to ${node._id}.`);
+        }
+      }
+    }
+    return this;
+  }
+
+  public disconnect(...nodes: Array<Node<OutT, unknown>>): typeof this {
+    for (const node of nodes) {
+      if (this._stream instanceof Readable && node._stream instanceof Writable) {
+        this._stream.unpipe(node._stream);
+        this._stream.resume();
+        if (Config.debug) {
+          console.log(`Disconnected ${this._id} from ${node._id}.`);
+        }
+      }
+    }
+    return this;
+  }
+
+  protected async _write(data: InT, encoding?: NodeJS.BufferEncoding): Promise<void> {
+    if (this._stream.closed || !(this._stream instanceof Writable))
+      return;
+
+    if (this._stream.writableNeedDrain) {
+      this._queue.push(data);
+      this._size += this._stream.writableObjectMode ? 1 : (<string | Buffer>data).length ?? (<DataView>data).byteLength ?? 0;
+      return;
     }
 
-    public connect(...nodes: Array<Node<OutT, unknown>>): typeof this {
-        for (const node of nodes) {
-            if (this._stream instanceof Readable && node._stream instanceof Writable) {
-                this._stream?.pipe(node._stream);
-                if (node._stream instanceof Readable) {
-                    node._stream.resume();
-                }
-                if (Config.debug) {
-                    console.log(`Connected ${this._id} to ${node._id}.`);
-                }
-            }
-        }
-        return this;
-    }
+    if (this._stream.write(data, encoding ?? 'utf-8'))
+      return;
 
-    public disconnect(...nodes: Array<Node<OutT, unknown>>): typeof this {
-        for (const node of nodes) {
-            if (this._stream instanceof Readable && node._stream instanceof Writable) {
-                this._stream.unpipe(node._stream);
-                this._stream.resume();
-                if (Config.debug) {
-                    console.log(`Disconnected ${this._id} from ${node._id}.`);
-                }
-            }
-        }
-        return this;
-    }
+    await once(this._stream, 'drain');
 
-    protected async _write(data: InT, encoding?: NodeJS.BufferEncoding): Promise<void> {
-        if (this._stream.closed || !(this._stream instanceof Writable))
-            return;
-
-        if (this._stream.writableNeedDrain) {
-            this._queue.push(data);
-            this._size += this._stream.writableObjectMode ? 1 : (<string|Buffer>data).length ?? (<DataView>data).byteLength ?? 0; 
-            return;
-        }
-
-        if (this._stream.write(data, encoding ?? 'utf-8'))
-            return;
-
+    while (this._queue.length) {
+      const data = this._queue.shift();
+      this._size -= this._stream.writableObjectMode ? 1 : (<string | Buffer>data).length ?? (<DataView>data).byteLength ?? 0;
+      if (!this._stream.write(data, encoding ?? 'utf-8')) {
         await once(this._stream, 'drain');
-
-        while (this._queue.length) {
-            const data = this._queue.shift();
-            this._size -= this._stream.writableObjectMode ? 1 : (<string|Buffer>data).length ?? (<DataView>data).byteLength ?? 0;
-            if (!this._stream.write(data, encoding ?? 'utf-8')) {
-                await once(this._stream, 'drain');
-            }
-        }
+      }
     }
+  }
 }
